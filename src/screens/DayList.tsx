1bc74f29664ee50468ebf1813114d3c950452183
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2024-04-03 22:29:40 +0900

    📌checkpoint: modal to navigate different screens

--- src/screens/DayList.tsx
+++ src/screens/DayList.tsx
@@ -1,16 +1,84 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  Modal,
+  Pressable,
+  TouchableWithoutFeedback,
+} from "react-native";
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "@/types";
+import { useState } from "react";
 
 type Props = NativeStackScreenProps<RootStackParamList, "DayList">;
 
 export default function DayList({ navigation }: Props) {
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
     <View style={styles.container}>
       <Text>DayList</Text>
+      <View
+        style={{
+          position: "absolute",
+          display: modalVisible ? "flex" : "none",
+          width: "100%",
+          height: "100%",
+          backgroundColor: "rgba(0, 0, 0, 0.2)",
+          justifyContent: "flex-end",
+        }}></View>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+        style={{}}>
+        <TouchableWithoutFeedback
+          onPress={() => {
+            setModalVisible(false);
+          }}>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "flex-end",
+            }}>
+            <View
+              style={{
+                backgroundColor: "white",
+                padding: 20,
+                borderTopLeftRadius: 10,
+                borderTopRightRadius: 10,
+
+                // width: "100%",
+                height: "30%",
+                // borderRadius: 10,
+                // alignItems: "center",
+                // justifyContent: "center",
+                // borderWidth: 2,
+                // borderStyle: "solid",
+                // backgroundColor: "white",
+              }}>
+              <Text>Modal</Text>
+              <Button
+                title="Close"
+                onPress={() => setModalVisible(!modalVisible)}
+              />
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </Modal>
       <Button
         title="Go to DayEdit"
         onPress={() => navigation.navigate("DayEdit")}
+      />
+      <Button
+        title="show modal"
+        onPress={() => {
+          setModalVisible(true);
+        }}
       />
     </View>
   );
